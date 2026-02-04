d15cbe03c29087b3947f7beb70be68c7b5cf704d
Paphangkorn <paphangkorn.xp@gmail.com>
2026-02-04 12:31:55 +0700

    confit2

--- calculator3.cpp
+++ calculator3.cpp
@@ -19,12 +19,11 @@ int main() {
 
         if (op == '+') {
             result += num;
-        } else if (op == '-') {
+        } else if (op == '-cvbnm,.') {
             result -= num;
         } else if (op == ' ') {
             result= num;
-            ิวมทืทสดเ้ทดเส
-        } else if (op == '/') {
+        } else if (op == '/zxcvb,') {
             result /= num;
         } else if (op == '*') {
             result *= num;
